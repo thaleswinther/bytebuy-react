59228056ad6ef2918f1235909f453b65bbec0b16
thales <thaleswcm@estudante.ufscar.br>
2024-01-24 23:16:49 -0300

    feat: adicionando icon da shopping bag e ajustando a navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,7 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
+import React from "react";
 import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Image from "react-bootstrap/Image";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import logotipo from "../assets/bytebuy_logo.svg";
@@ -8,20 +9,21 @@ import sacola_compras from "../assets/shopping_bag.svg";
 
 function NavBar() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand href="/">
-          <img src={logotipo} alt="Byte Buy" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav>
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/contato">Contato</Nav.Link>
-            <Nav.Link href="/localizacao">Localização</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+    <Navbar collapseOnSelect expand="lg" bg="black" variant="dark" style={{ height: '108px' }}>
+      <Navbar.Brand href="/" style={{ marginLeft: '20px' }}>
+        <img src={logotipo} alt="Byte Buy" style={{ width: '100px', height: 'auto'}} />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="ms-auto align-items-center">
+          <Nav.Link href="/" className="text-white">Home</Nav.Link>
+          <Nav.Link href="/contato" className="text-white">Contato</Nav.Link>
+          <Nav.Link href="/localizacao" className="text-white">Localização</Nav.Link>
+          <Nav.Link href="/" className="text-white d-flex align-items-center" style={{ marginRight: '20px' }}>
+            <Image src={sacola_compras} alt="Carrinho" fluid style={{ width: '32px', height: 'auto' }} />
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
